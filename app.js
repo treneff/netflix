c12085ca4d907b420788ac9fca6df16ad0f7f690
George Treneff <treneff@protonmail.com>
2020-08-10 21:24:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-// Select tab content item
+//adding content and update DOM item tabs
 function selectItem(e) {
-    // Remove all show and border classes
+
     removeBorder();
     removeShow();
-    // Add border to current tab item
     this.classList.add('tab-border');
-    // Grab content item from DOM
-    const tabContentItem = document.querySelector(`#${this.id}-content`);
-    // Add show class
+    const tabContentItem = document.querySelector(`#.${this.id}-content`);
     tabContentItem.classList.add('show');
 }
 
-// Remove bottom borders from all tab items
+//remove borders
 removeBorder = () => {
     tabItems.forEach(item => {
         item.classList.remove('tab-border');
     });
 }
 
-// Remove show class from all content items
+// remove content on click
 removeShow = () => {
     tabContentItems.forEach(item => {
         item.classList.remove('show');
     });
 }
 
-// Listen for tab item click
+//listen for clicks
 tabItems.forEach(item => {
     item.addEventListener('click', selectItem);
 });
